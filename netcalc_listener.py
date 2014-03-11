64c008a691ff9da08d355a9032372c3d2517bccb
Chris Antenesse <chris@antenesse.net>
2014-03-11 16:37:04 -0700

    removing lib, no need to be fancy about this

--- netcalc_listener.py
+++ netcalc_listener.py
@@ -4,9 +4,6 @@ import json
 import IPy
 import sys
 
-sys.path.append('lib')
-import netcalc
-
 def calculate_network_info(net_input):
     net_input_json = json.loads(net_input)
     nc = netcalc.NetCalc(net_input_json)
@@ -16,6 +13,7 @@ def calculate_network_info(net_input):
 class APIHandler(tornado.web.RequestHandler):
     def post(self):
         self.write(calculate_network_info(self.request.body))
+
 
 class FormHandler(tornado.web.RequestHandler):
     def get(self):
@@ -29,6 +27,23 @@ class FormHandler(tornado.web.RequestHandler):
         self.render("templates/output.html", title="Subnet Calc",
                     net_dict=blah)
         #self.write(calculate_network_info(net_dict))
+
+class NetCalc:
+    def __init__(self, network_struct):
+        self.ip = network_struct['ip']
+        self.netmask = network_struct['netmask']
+        
+    def calc(self):
+        network = IPy.IP(self.ip).make_net(self.netmask)
+        network_id = network.strNormal(0)
+        network_cidr = network.strNormal(1)
+        network_range = network.strNormal(3)
+       
+        network_dict = {'net_cidr': network_cidr, 'network_id': network_id,
+                        'network_range': network_range}
+
+        return network_dict
+
          
 application = tornado.web.Application([
     (r"/netcalc/api", APIHandler),
