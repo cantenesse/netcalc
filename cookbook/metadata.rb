76564f52feab506d101169b2a98f2f5c11289002
Chris Antenesse <chris@antenesse.net>
2014-05-05 16:29:29 -0700

    added cookbook metadata

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -7,3 +7,13 @@ long_description 'Installs/Configures netcalc'
 version File.read(File.join(File.dirname(__FILE__), "..", "VERSION"))
 
 depends 'apt'
+
+recipe 'netcalc::default', 'Installs a full stack netcalc server'
+recipe 'netcalc::app', 'Installs a netcalc application server'
+
+attribute 'netcalc/app/port'
+    display_name: 'App Server Port',
+    description: 'Listen port for application server',
+    type: 'integer'
+    default: 8888
+    
